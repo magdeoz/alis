77daab048fa3d3147124fb4cdcf194ea8195ee12
magdeoz <hackmagodeoz@gmail.com>
2019-05-30 15:42:56 -0500

    fix lol

--- download.sh
+++ download.sh
@@ -7,13 +7,6 @@ set -e
 
 rm -f alis.conf
 rm -f alis.sh
-rm -f alis-asciinema.sh
-rm -f alis-reboot.sh
-
-rm -f alis-recovery.conf
-rm -f alis-recovery.sh
-rm -f alis-recovery-asciinema.sh
-rm -f alis-recovery-reboot.sh
 
 wget https://raw.githubusercontent.com/magdeoz/alis/master/alis.conf
 wget https://raw.githubusercontent.com/magdeoz/alis/master/alis.sh
